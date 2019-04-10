6a93c79637afe309e8bb0188ff4eac6a6a67a277
JuanS09 <Juanmanuelsierra311@gmail.com>
2019-04-10 11:59:31 -0600

    creacion de Router

--- src/components/router.js
+++ src/components/router.js
@@ -3,22 +3,25 @@ import {
     BrowserRouter as Router,
     Route,
 } from 'react-router-dom';
+import './globales.css';
 
 import IniciarSesion from './inicioDeSesion/entrar';
 import Registrarse from './boleto/Registro/Regristrarse';
 import CrearBoleto from './../containers/boleto/crearContainer';
 import Descripcion from './boleto/descripcion';
 import Contactenos from './boleto/contacto';
+import Inicio from './PantallaInicio/inicio';
 
 const RouterApp = () => {
     return (
         <Router>
             <Fragment>
-                <Route path="/" exact component={IniciarSesion} />
+                <Route path="/IniciarSecion" exact component={IniciarSesion} />
                 <Route path="/registrarse" exact component={Registrarse} />
-                <Route path="/boleto/crear" exact component={CrearBoleto} />
+                <Route path="/boleto/crear/:transporte" exact component={CrearBoleto} />
                 <Route path="/boleto/descripcion" exact component={Descripcion} />
                 <Route path="/boleto/contacto" exact component={Contactenos} />
+                <Route path="/" exact component={Inicio} />
             </Fragment>
         </Router>
     )
